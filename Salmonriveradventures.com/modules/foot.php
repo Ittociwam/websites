5cb6ba64a6ebe657a0e1caf07879bedfa9868b86
Robbie Bise <ittociwam@hotmail.com>
2014-12-09 11:34:43 -0700

    adding code for my websites

---
+++ Salmonriveradventures.com/modules/foot.php
@@ -0,0 +1,8 @@
+<nav class="grid" >
+    <ul class='unit whole grid'>
+        <li class='unit one-third'><a href="/salmon/foot-nav/site-plan.php" title="Read the Site Plan for Salmon River Adventures">Site Plan</a> </li>
+        <li class='unit one-third'><a href="/salmon/foot-nav/contact/contact.php" title="Sign up for a river rafting trip today!">Contact Us</a> </li>
+        <li class='unit one-third'><a href="/salmon/foot-nav/resources.php" title="Resources"> Resources</a> </li>
+    </ul>
+
+</nav>
