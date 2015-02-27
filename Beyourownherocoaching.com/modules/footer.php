ea2936ffc4cdd0b935d299647750b040d5025407
Robbie Bise <ittociwam@hotmail.com>
2015-02-27 13:44:33 -0700

    can now use WAMP to view site on local machine
    Signed-off-by: Robbie Bise <ittociwam@hotmail.com>

--- Beyourownherocoaching.com/modules/footer.php
+++
@@ -1,8 +0,0 @@
-<nav class="footnav" >
-    <ul>
-        <li><a href="/foot-nav/site-plan/site-plan.php" title="Read the Site Plan for Beyourownherocoaching.com">Site Plan</a> </li>
-        <li><a href="/foot-nav/contact/contact.php" title="We would love to hear your comments!">Contact Us</a> </li>
-        <li><a href="/foot-nav/presentation/presentation.php" title="Presentation for CIT 230"> CIT 230 Presentation</a> </li>
-    </ul>
-    
-</nav>
